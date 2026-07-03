f01c78112158fa8b75a822ab66365f9eaee206ee
yaniard17 <yaniardiansyah12@gmail.com>
2026-07-03 19:14:18 +0700

    config.php

--- presensi-online/config.php
+++ presensi-online/config.php
@@ -5,6 +5,8 @@ $pass = "";
 $db   = "presensi_online";
 
 $conn = new mysqli($host, $user, $pass, $db);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
